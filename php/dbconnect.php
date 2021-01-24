1fc5e92fc919f72dac16d2a7de378d61bf1b5b57
WeiDi <weiditan@hotmail.com>
2021-01-24 16:01:35 +0800

    update

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 
 	$dbservername = "localhost";
-	$dbusername = "u198522155_doktorsaya";
-	$dbpassword = "2gjv0h#Y";
-	$dbdatabase = "u198522155_doktorsaya";
+	$dbusername = "";
+	$dbpassword = "";
+	$dbdatabase = "";
 
 	// Create connection
 	$conn = new mysqli($dbservername, $dbusername, $dbpassword, $dbdatabase);
